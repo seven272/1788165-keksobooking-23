fd59988ba8fb9c2256a2a1ea4959abfa406d639b
Mikhail <i727i@mail.ru>
2021-06-02 15:54:38 +0300

    Основные возможности JavaScript (переменные через const)

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
-let randomNumber = function (min, max) {
+const randomNumber = function (min, max) {
     if (min < 0) {
         return window.alert("Недопустимое значение. Введите корректоное число, которое больше нуля")
     }
     return Math.floor(Math.random() * (max - min) + min);
 }
 
-randomNumber(3, 15);
+console.log(randomNumber(5, 65));
 
 
-let randomNumberDecimal = function(min, max, decimalPlaces) {
+const randomNumberDecimal = function(min, max, decimalPlaces) {
     if (min < 0) {
         return window.alert("Недопустимое значение. Введите корректоное число, которое больше нуля")
     }
@@ -17,4 +17,4 @@ let randomNumberDecimal = function(min, max, decimalPlaces) {
     return ~~ (randomValue * decimalConverter) / decimalConverter;
 }
 
-randomNumberDecimal(12.4562566, 12.6705, 2);
+console.log(randomNumberDecimal(17.4262566, 82.1705, 4));
