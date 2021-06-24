ba0713cb1e3e1392597607d57bb90205f44ec1bb
Mikhail <i727i@mail.ru>
2021-06-24 18:09:10 +0300

    режим активной/неактивной страницы

--- js/modules/form.js
+++ js/modules/form.js
@@ -1,8 +1,74 @@
 const adForm = document.querySelector('.ad-form');
+//переменные интерактивных элементов формы 
 const selectRooms = adForm.querySelector('#room_number');
 const selectGuests = adForm.querySelector('#capacity');
 const selectTimeIn = adForm.querySelector('#timein');
 const selectTimeOut = adForm.querySelector('#timeout');
+
+const selectFormElements = adForm.querySelectorAll('.ad-form__element');
+const mapForm = document.querySelector('.map__filters');
+const selectMapElements = mapForm.querySelectorAll('.map__filter');
+const selectMapElementsFeatures = mapForm.querySelectorAll('.map__checkbox');
+
+// console.log(selectFormElements);
+
+
+//Режим активной / неактивной страницы
+
+//Функция скрытия элементов форм при неактивном режиме страницы
+let setNotActive = function() {
+  adForm.classList.add('ad-form--disabled');
+  mapForm.classList.add('map__filters--disabled');
+  selectFormElements.forEach (function(value) {
+    value.disabled = true;
+  
+  });
+  selectMapElements.forEach (function(value) {
+    value.disabled = true;
+  });
+
+  selectMapElementsFeatures.forEach (function(value) {
+    value.disabled = true;
+  });
+};
+setNotActive();
+
+//функуия активации элементов формы в режиме активной страницы
+let setPageState = function() {
+  adForm.classList.remove('ad-form--disabled');
+  mapForm.classList.remove('map__filters--disabled');
+  selectFormElements.forEach (function(value) {
+    value.disabled = false;
+  
+  });
+  selectMapElements.forEach (function(value) {
+    value.disabled = false;
+  });
+
+  selectMapElementsFeatures.forEach (function(value) {
+    value.disabled = false;
+  });
+
+}
+setPageState();
+
+//С помощью функции Array.from преобразуем псевдомассив в массив
+// let arraySelectFormElements = Array.from(selectFormElements);
+// let arraySelectMapElements = Array.from(selectMapElements);
+// console.log(arraySelectFormElements)
+//Обьеденяем два массива в один
+// let arrayFormAndMap = arraySelectFormElements.concat(arraySelectMapElements);
+// console.log(arrayFormAndMap)
+//пытаемся с помощью функции в качестве параметра, которой передан флаг менять значения.
+// let setPageState = function(active) {
+//    for(let i = 0; i < active.length;i++) {
+//      if(active[i].disabled = false) {
+//       return true;
+//      }  
+//     //  return false;
+//    }
+//  }
+//  setPageState(arrayFormAndMap);
 
 //Зависимость количества комнат от количества жильцов
 selectRooms.addEventListener('change', (evt) => {
